4ec5c1bc96e1c7ff77776138d41375bdc6f2a16c
mitrakov <mitrakov-artem@yandex.ru>
2017-03-09 22:19:34 +0500

    Little bit changed Android Launcher

--- android/src/ru/mitrakov/self/rush/AndroidLauncher.java
+++ android/src/ru/mitrakov/self/rush/AndroidLauncher.java
@@ -4,8 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.graphics.Rect;
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.backends.android.*;
 
 public class AndroidLauncher extends AndroidApplication {
     @Override
@@ -17,13 +16,12 @@ public class AndroidLauncher extends AndroidApplication {
         config.useGyroscope = false;
 
         final PsObject obj = new PsObject();
-        final View view = getWindow().getDecorView().getRootView();
         // @mitrakov: "addOnLayoutChangeListener" requires Level API 11
-        view.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+        getWindow().getDecorView().getRootView().addOnLayoutChangeListener(new View.OnLayoutChangeListener() { //no NULL
             @Override
-            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int a, int b, int c, int d) {
                 Rect rect = new Rect();
-                view.getWindowVisibleDisplayFrame(rect);
+                v.getWindowVisibleDisplayFrame(rect);
                 System.out.println("New Size: " + rect.width() + "; " + rect.height());
                 obj.raiseRatioChanged(1f * rect.width() / rect.height());
             }
