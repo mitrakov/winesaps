a0a4641aa27e270dbb53f691cfd06080ad045bc2
mitrakov <mitrakov-artem@yandex.ru>
2017-07-26 23:24:42 +0500

    Added 1) sound; 2) positioning for Desktop Push Notifications

--- desktop/src/ru/mitrakov/self/rush/desktop/DesktopPsObject.java
+++ desktop/src/ru/mitrakov/self/rush/desktop/DesktopPsObject.java
@@ -1,5 +1,7 @@
 package ru.mitrakov.self.rush.desktop;
 
+import java.awt.*;
+
 import javax.swing.*;
 
 import static javax.swing.JOptionPane.*;
@@ -26,9 +28,32 @@ class DesktopPsObject extends PsObject {
     @Override
     public void pushNotification(String msg) {
         if (!parent.isVisible()) { // if the app is active => no need to push notifications
-            int res = showOptionDialog(null, msg, "Winesaps", OK_CANCEL_OPTION, QUESTION_MESSAGE, icon, null, null);
-            if (res == OK_OPTION)
-                parent.setVisible(true);
+            JOptionPane pane = new JOptionPane(msg, QUESTION_MESSAGE, OK_CANCEL_OPTION, icon);
+            JDialog dialog = pane.createDialog(null, "Winesaps");
+            locateToRightBottom(dialog);
+            Object sound = Toolkit.getDefaultToolkit().getDesktopProperty("win.sound.default"); //TODO only on Windows?
+            if (sound instanceof Runnable)
+                ((Runnable) sound).run();
+            dialog.setVisible(true); // this call blocks the execution!
+            dialog.dispose();
+            Object value = pane.getValue();
+            if (value instanceof Integer)
+                if ((Integer) value == OK_OPTION)
+                    parent.setVisible(true);
         }
+    }
+
+    /**
+     * See http://stackoverflow.com/questions/9753722
+     *
+     * @param f - component to locate
+     */
+    private void locateToRightBottom(Component f) {
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice defaultScreen = ge.getDefaultScreenDevice();
+        Rectangle rect = defaultScreen.getDefaultConfiguration().getBounds();
+        int x = (int) rect.getMaxX() - f.getWidth();
+        int y = (int) rect.getMaxY() - f.getHeight() - 50;
+        f.setLocation(x, y);
     }
 }
