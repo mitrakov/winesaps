a8305a79799506966106376bfc7bd1213a4d9be5
mitrakov <mitrakov-artem@yandex.ru>
2017-08-10 15:19:08 +0500

    Simplified "Close-on-Esc" dialog feature (it turned out there is a single line solution in LibGDX API)

--- core/src/ru/mitrakov/self/rush/ui/DialogFeat.java
+++ core/src/ru/mitrakov/self/rush/ui/DialogFeat.java
@@ -1,10 +1,8 @@
 package ru.mitrakov.self.rush.ui;
 
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 import ru.mitrakov.self.rush.Localizable;
 
@@ -15,7 +13,7 @@ public abstract class DialogFeat extends Dialog implements Localizable {
     public static void hideAll(Stage stage) {
         assert stage != null;
         for (Actor actor : stage.getActors()) {
-            if (actor instanceof Window)
+            if (actor instanceof Dialog)
                 actor.remove(); // It is SAFE! See SnapshotArray<T>
         }
     }
@@ -24,26 +22,8 @@ public abstract class DialogFeat extends Dialog implements Localizable {
 
     public DialogFeat(String title, Skin skin, String windowStyleName) {
         super(title, skin, windowStyleName);
-
-        // set up button styles
         getButtonTable().defaults().minWidth(120).height(46).padLeft(10).padRight(10);
-
-        // add 'close-on-esc' feature
-        addListener(new InputListener() {
-            @Override
-            public boolean keyDown(InputEvent event, int keycode) {
-                Array<Actor> children = getButtonTable().getChildren();
-                assert children != null;
-                if (keycode == Input.Keys.ESCAPE || (keycode == Input.Keys.ENTER && children.size == 1)) {
-                    // programmatically 'push' the last button
-                    if (children.size > 0) {
-                        assert children.peek() != null;
-                        children.peek().fire(new ChangeListener.ChangeEvent());
-                    }
-                }
-                return super.keyDown(event, keycode);
-            }
-        });
+        key(Input.Keys.ESCAPE, null);
     }
 
     @Override
