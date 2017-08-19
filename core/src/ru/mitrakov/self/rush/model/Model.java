4933b84a01b747ab596b4700290513f401a89acb
mitrakov <mitrakov-artem@yandex.ru>
2017-08-19 14:48:33 +0500

    Replesed: 1) Snowshoes with Sapper Shoes; 2) Spy with Teleport Man

--- core/src/ru/mitrakov/self/rush/model/Model.java
+++ core/src/ru/mitrakov/self/rush/model/Model.java
@@ -139,10 +139,10 @@ public class Model {
      * ability list; some abilities are stubs (a7, a8, up to a32), because skills start with an index=33
      */
     public enum Ability {
-        None, Snorkel, ClimbingShoes, SouthWester, VoodooMask, Snowshoes, Sunglasses,
+        None, Snorkel, ClimbingShoes, SouthWester, VoodooMask, SapperShoes, Sunglasses,
         a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19,
         a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32,
-        Miner, Builder, Shaman, Grenadier, Spy
+        Miner, Builder, Shaman, Grenadier, TeleportMan
     }
 
     public final Ability[] abilityValues = Ability.values();
